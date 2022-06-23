089bc302eb5129b5c21e3fbe2941985d0a9e9644
Alec Torres <atorresla24@gmail.com>
2022-06-23 02:21:46 -0400

    lab7

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ console.log('working');
 
 const express = require('express');
 require('dotenv').config();
-require('data/weather.json').config();
-
-app.get('/weather', (request, response) => {
-  console.log(description);
-  let description = request.query.description;
-  response.send()
-})
+let data = require('./data/weather.json');
 
 
 const app = express();
+
+class Forecast{
+  constructor(weatherObj){
+    this.datetime = weatherObj.datetime;
+    this.description = weatherObj.description;
+  }
+}
+
+app.get('/weather', (request, response) => {
+  const searchQuery = request.query.searchQuery;
+  console.log(searchQuery);
+  let searchResult = data.find(object => object.city_name === searchQuery);
+  response.send(searchQuery);
+
+  const result = new Forecast(searchResult);
+  console.log(result);
+})
 
 const PORT = process.env.PORT || 3002;
 
