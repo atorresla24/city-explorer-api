050f36d4b26630f06c665291474eea66f1339cb1
Alec Torres <atorresla24@gmail.com>
2022-06-29 03:57:20 -0400

    lab9

--- modules/weather.js
+++ modules/weather.js
@@ -1,73 +1,77 @@
+// 'use strict';
+// const axios = require('axios');
+
+// async function getWeather(request, response, next){
+//   try{
+//     const searchQuery = request.query.searchQuery;
+//     console.log(searchQuery);
+//     let lat = request.query.lat;
+//     console.log(lat);
+//     let lon = request.query.lon;
+//     console.log(lon);
+//     let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&units=I&days=5&lat=${lat}&lon=${lon}`;
+//     let weatherInfo = await axios.get(url);
+//     let weatherData = weatherInfo.data.data.map(sky => new Forecast(sky));
+//     //let dataSend = searchResult.data.data.map(day => new Forecast(day));
+//     //response.send(dataSend);
+//     response.status(200).send(weatherData);
+//   } catch (error) {
+//     console.log('getWeather error');
+//     console.log(error);
+//     next (error);
+//   }
+// }
+
+// class Forecast{
+//   constructor(weatherObj){
+//     this.datetime = weatherObj.datetime;
+//     this.description = weatherObj.weather.description;
+//   }
+// }
+
+// module.exports = getWeather;
+
 'use strict';
+
+let cache = require('./cache.js');
 const axios = require('axios');
 
-async function getWeather(request, response, next){
-  try{
-    const searchQuery = request.query.searchQuery;
-    console.log(searchQuery);
-    let lat = request.query.lat;
-    console.log(lat);
-    let lon = request.query.lon;
-    console.log(lon);
-    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&units=I&days=5&lat=${lat}&lon=${lon}`;
-    let weatherInfo = await axios.get(url);
-    let weatherData = weatherInfo.data.data.map(sky => new Forecast(sky));
-    //let dataSend = searchResult.data.data.map(day => new Forecast(day));
-    //response.send(dataSend);
-    response.status(200).send(weatherData);
-  } catch (error) {
-    console.log('getWeather error');
-    console.log(error);
-    next (error);
+async function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + longitude;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${latitude}&lon=${longitude}&days=5`;
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(response => parseWeather(response.data));
+  }
+
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
   }
 }
 
-class Forecast{
-  constructor(weatherObj){
-    this.datetime = weatherObj.datetime;
-    this.description = weatherObj.weather.description;
+class Weather {
+  constructor(day) {
+    this.description = day.weather.description;
+    this.date = day.datetime;
+    this.temp = day.temp;
+    this.min_temp = day.min_temp;
+    this.max_temp = day.max_temp;
   }
 }
 
 module.exports = getWeather;
-
-// 'use strict';
-
-// let cache = require('./cache.js');
-
-// module.exports = getWeather;
-
-// function getWeather(latitude, longitude) {
-//   const key = 'weather-' + latitude + longitude;
-//   const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
-
-//   if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
-//     console.log('Cache hit');
-//   } else {
-//     console.log('Cache miss');
-//     cache[key] = {};
-//     cache[key].timestamp = Date.now();
-//     cache[key].data = axios.get(url)
-//     .then(response => parseWeather(response.data));
-//   }
-
-//   return cache[key].data;
-// }
-
-// function parseWeather(weatherData) {
-//   try {
-//     const weatherSummaries = weatherData.data.map(day => {
-//       return new Weather(day);
-//     });
-//     return Promise.resolve(weatherSummaries);
-//   } catch (e) {
-//     return Promise.reject(e);
-//   }
-// }
-
-// class Weather {
-//   constructor(day) {
-//     this.forecast = day.weather.description;
-//     this.time = day.datetime;
-//   }
-// }
--- server.js
+++ server.js
@@ -4,16 +4,40 @@ console.log('working');
 
 const express = require('express');
 require('dotenv').config();
+//let data = require('./data/weather.json');
 const cors = require('cors');
+//const axios = require('axios');
+const weather = require('./modules/weather.js');
+const movies = require('./modules/movies');
+
 
 const app = express();
 app.use(cors());
 
-const getWeather = require('./modules/weather.js');
 app.get('/weather', getWeather);
+app.get('./movies', getMovies);
 
-const getMovies = require('./modules/movies.js');
-app.get ('/movies', getMovies);
+function getWeather(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!');
+    });
+}
+
+function getMovies(request, response){
+  const city = request.query.searchQueryCity;
+  movies(city).then(summaries => response.send (summaries)).catch((error) => {
+    console.log(error);
+    response.status(200).send('Sorry. Something went wrong getting movies!');
+  });
+}
+
+// app.use((error, request, response) => {
+//   response.status(500).send(error.message);
+// });
 
 const PORT = process.env.PORT || 3002;
 
@@ -26,8 +50,60 @@ app.get('*', (request, response) => {
 });
 
 
-app.listen(PORT, () => {console.log(`listening on port ${PORT}`);
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`);
 });
+
+// class Forecast {
+//   constructor(weatherObj) {
+//     this.datetime = weatherObj.datetime;
+//     this.description = weatherObj.weather.description;
+//   }
+// }
+
+// app.get('/weather', async (request, response) => {
+//   try {
+//     const searchQuery = request.query.searchQuery;
+//     console.log(searchQuery);
+//     let lat = request.query.lat;
+//     console.log(lat);
+//     let lon = request.query.lon;
+//     console.log(lon);
+//     //let searchResult = data.find(object => object.city_name === searchQuery);
+//     let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&units=I&days=5&lat=${lat}&lon=${lon}`;
+//     let weatherInfo = await axios.get(url);
+//     let weatherData = weatherInfo.data.data.map(sky => new Forecast(sky));
+//     response.send(weatherData);
+//     //let dataSend = searchResult.data.data.map(day => new Forecast(day));
+//     //response.send(dataSend);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   //const result = new Forecast(searchResult);
+//   //console.log(result);
+// });
+
+// app.get('/movies', getMovies);
+
+// async function getMovies(request, response) {
+//   try {
+//     let searchQuery = request.query.searchQuery;
+//     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+//     let cityMovie = await axios.get(url);
+//     let movieArray = cityMovie.data.results.map(movie => new Movie(movie));
+//     //console.log(cityMovie.data);
+//     response.send(movieArray);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// class Movie {
+//   constructor(movieObj) {
+//     this.title = movieObj.title;
+//     this.description = movieObj.overview;
+//   }
+// }
 
 // 'use strict';
 
@@ -38,16 +114,17 @@ app.listen(PORT, () => {console.log(`listening on port ${PORT}`);
 // const weather = require('./modules/weather.js');
 // const app = express();
 
+// app.use(cors());
 // app.get('/weather', weatherHandler);
 
 // function weatherHandler(request, response) {
 //   const { lat, lon } = request.query;
 //   weather(lat, lon)
-//   .then(summaries => response.send(summaries))
-//   .catch((error) => {
-//     console.error(error);
-//     response.status(200).send('Sorry. Something went wrong!')
-//   });
+//     .then(summaries => response.send(summaries))
+//     .catch((error) => {
+//       console.error(error);
+//       response.status(200).send('Sorry. Something went wrong!');
+//     });
 // }
 
 // app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
